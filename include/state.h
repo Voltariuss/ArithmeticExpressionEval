bc86d198082c9fd5e2c52011f6a158ad8b1f2b84
Voltariuss <loic.duboistermoz@gmail.com>
2021-02-03 00:19:00 +0100

    adds states clases 1 to 9

--- include/state.h
+++ include/state.h
@@ -23,3 +23,84 @@ public:
   bool transition(Automate *automate, Symbol *symbol) const;
   void print() const;
 };
+
+class State1 : public State
+{
+public:
+  State1() : State() {}
+  ~State1() {}
+  bool transition(Automate *automate, Symbol *symbol) const;
+  void print() const;
+};
+
+class State2 : public State
+{
+public:
+  State2() : State() {}
+  ~State2() {}
+  bool transition(Automate *automate, Symbol *symbol) const;
+  void print() const;
+};
+
+class State3 : public State
+{
+public:
+  State3() : State() {}
+  ~State3() {}
+  bool transition(Automate *automate, Symbol *symbol) const;
+  void print() const;
+};
+
+class State4 : public State
+{
+public:
+  State4() : State() {}
+  ~State4() {}
+  bool transition(Automate *automate, Symbol *symbol) const;
+  void print() const;
+};
+
+class State5 : public State
+{
+public:
+  State5() : State() {}
+  ~State5() {}
+  bool transition(Automate *automate, Symbol *symbol) const;
+  void print() const;
+};
+
+class State6 : public State
+{
+public:
+  State6() : State() {}
+  ~State6() {}
+  bool transition(Automate *automate, Symbol *symbol) const;
+  void print() const;
+};
+
+class State7 : public State
+{
+public:
+  State7() : State() {}
+  ~State7() {}
+  bool transition(Automate *automate, Symbol *symbol) const;
+  void print() const;
+};
+
+class State8 : public State
+{
+public:
+  State8() : State() {}
+  ~State8() {}
+  bool transition(Automate *automate, Symbol *symbol) const;
+  void print() const;
+};
+
+class State9 : public State
+{
+public:
+  State9() : State() {}
+  ~State9() {}
+  bool transition(Automate *automate, Symbol *symbol) const;
+  void print() const;
+};
--- src/state.cpp
+++ src/state.cpp
@@ -9,3 +9,84 @@ bool State0::transition(Automate *automate, Symbol *symbol) const
 void State0::print() const {
   cout << "State0" << endl;
 }
+
+bool State1::transition(Automate *automate, Symbol *symbol) const
+{
+  // TODO
+}
+
+void State1::print() const {
+  cout << "State1" << endl;
+}
+
+bool State2::transition(Automate *automate, Symbol *symbol) const
+{
+  // TODO
+}
+
+void State2::print() const {
+  cout << "State2" << endl;
+}
+
+bool State3::transition(Automate *automate, Symbol *symbol) const
+{
+  // TODO
+}
+
+void State3::print() const {
+  cout << "State3" << endl;
+}
+
+bool State4::transition(Automate *automate, Symbol *symbol) const
+{
+  // TODO
+}
+
+void State4::print() const {
+  cout << "State4" << endl;
+}
+
+bool State5::transition(Automate *automate, Symbol *symbol) const
+{
+  // TODO
+}
+
+void State5::print() const {
+  cout << "State5" << endl;
+}
+
+bool State6::transition(Automate *automate, Symbol *symbol) const
+{
+  // TODO
+}
+
+void State6::print() const {
+  cout << "State6" << endl;
+}
+
+bool State7::transition(Automate *automate, Symbol *symbol) const
+{
+  // TODO
+}
+
+void State7::print() const {
+  cout << "State7" << endl;
+}
+
+bool State8::transition(Automate *automate, Symbol *symbol) const
+{
+  // TODO
+}
+
+void State8::print() const {
+  cout << "State8" << endl;
+}
+
+bool State9::transition(Automate *automate, Symbol *symbol) const
+{
+  // TODO
+}
+
+void State9::print() const {
+  cout << "State9" << endl;
+}
